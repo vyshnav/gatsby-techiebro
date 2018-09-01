d1d8fabdb3f0dcd69ff9b18a9e1475037d639535
vyshnav <vyshnav.rs@gmail.com>
2018-09-01 17:07:28 +0530

    post page edits

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,16 +7,24 @@ import { Header } from 'semantic-ui-react'
 const Wrapper = styled.section`
   position: relative;
   min-height: 300px;
+  @media (max-width: ${props => props.theme.responsive.medium}) {  
+    min-height: 200px;
+  }
 `
 const BgImg = styled(Img)`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  min-height: 300px;
+  min-height: 200px;
+
   height: auto;
   @media (min-width: ${props => props.theme.responsive.small}) {
+    height: ${props => props.height || 'auto'};   
+  }
+  @media (max-width: ${props => props.theme.responsive.medium}) {
     height: ${props => props.height || 'auto'};
+    max-height: 200px;
   }
   & > img {
     object-fit: ${props => props.fit || 'cover'} !important;
@@ -40,7 +48,7 @@ const TbHeader = styled(Header)`
   margin-bottom: 10px !important
   max-width: ${props => props.theme.sizes.maxWidthCentered};
 
-  font-size: 3em;
+  font-size: 1.67rem !important;
   text-transform: capitalize;
   font-weight: 600;
   position: absolute;
@@ -54,6 +62,10 @@ const TbHeader = styled(Header)`
   z-index: 1;
   @media only screen and (max-width: 700px){
     padding: 0 1rem !important;
+  }
+
+  @media screen and (min-width: 830px){
+    font-size: 2.5rem !important;
   }
   
 ` 
